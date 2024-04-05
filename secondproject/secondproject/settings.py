833df807def94136979cedccc78b3388f2670daf
SanikaBhor6 <140701674+SanikaBhor6@users.noreply.github.com>
2024-04-05 12:51:06 +0530

    Update settings.py

--- secondproject/secondproject/settings.py
+++ secondproject/secondproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o%gisul8^wt5pj(v!7patu%-#xfe0w%^s%&g)1)3w1m*^@dk74
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sanika123.pythonanywhere.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
